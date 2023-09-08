1a68886fb013f6260c7a454c1c4ee716f16b77f5
RosasSebastian2003 <rosassebastian2003@gmail.com>
2023-09-08 00:17:50 -0600

    2.2 prob 1

--- act2_2.rkt
+++ act2_2.rkt
@@ -1,8 +1,32 @@
 #lang racket
 
+;;Problema 1, insert everywhere twice
 (define (insert-everywhere-twice x lst)
-    (if (null? lst)
-        (list (list x x))
-        (cons (cons x (cons x lst)) (map (lambda (rest) (cons (car lst) rest)) (insert-everywhere-twice x (cdr lst))))
+    (cond 
+        ((null? lst) (list(list x x)))
+        (else (cons (append (list x x) lst)
+        (map (lambda (rest) (cons (car lst) rest))
+        (insert-everywhere-twice x (cdr lst)))))
     )
 )
+
+(displayln "Problema 1, Insertar un elemento dos veces en todas las partes de una lista")
+(displayln "ej. 1, 1 '()")
+(displayln (insert-everywhere-twice 1 '()))
+(newline)
+
+(displayln "ej. 2, 1 '(a)")
+(displayln (insert-everywhere-twice 1 '(a)))
+(newline)
+
+(displayln "ej. 3, 1 '(a b c)")
+(displayln (insert-everywhere-twice 1 '(a b c)))
+(newline)
+
+(displayln "ej. 4,  1 '(a b c d e)")
+(displayln (insert-everywhere-twice  1 '(a b c d e)))
+(newline)
+
+(displayln "ej. 5,  x '(1 2 3 4 5 6 7 8 9 10)")
+(displayln (insert-everywhere-twice 'x '(1 2 3 4 5 6 7 8 9 10)))
+(newline)
