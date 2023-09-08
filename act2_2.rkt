94b47b4388989a762e03e8e8728c9925099435c5
RosasSebastian2003 <rosassebastian2003@gmail.com>
2023-09-08 00:35:39 -0600

    2.2 prob 2

--- act2_2.rkt
+++ act2_2.rkt
@@ -30,3 +30,35 @@
 (displayln "ej. 5,  x '(1 2 3 4 5 6 7 8 9 10)")
 (displayln (insert-everywhere-twice 'x '(1 2 3 4 5 6 7 8 9 10)))
 (newline)
+
+;;Problema 2, encode
+(define (encode lst)
+    (define (encode-aux lst prev count)
+        (cond 
+            ((null? lst) '())
+            ((equal? (car lst) prev) (encode-aux (cdr lst) prev (+ count 1)))
+            (else
+                (cons prev (cons count (encode-aux (cdr lst) (car lst) 1)))
+            )
+        )
+    )
+
+    (if (null? lst) '() (encode-aux (cdr lst) (car lst) 1))
+)
+
+(displayln "Problema 2, Hallar el número de veces que se repite un elemento en una lista")
+(displayln "ej. 1, '()")
+(displayln (encode '()))
+(newline)
+
+(displayln "ej. 2, '(a a a a b c c a a d e e e e)")
+(displayln (encode '(a a a a b c c a a d e e e e)))
+(newline)
+
+(displayln "ej. 3, '(1 2 3 4 5)")
+(displayln (encode '(1 2 3 4 5)))
+(newline)
+
+(displayln "ej. 4, '(9 9 9 9 7 7))")
+(displayln (encode '(9 9 9 9 7 7)))
+(newline)
