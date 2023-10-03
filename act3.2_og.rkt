f084d6a29d3524a99d0a077177a76c641d167383
RosasSebastian2003 <111817772+rosassebastian2@users.noreply.github.com>
2023-10-03 07:41:34 -0600

    Codigo funcional, falta lectura de un archivo

--- act3.2_og.rkt
+++ act3.2_og.rkt
@@ -1,44 +1,30 @@
 #lang racket
-;;Anatomia de la tabla de estados
-;;;Cada lista representa un estado
-;;;Cada indice de la cada lista de la tabla de estados representa un grupo de caracteres o un solo caracter
-;;;Cada indice de la lista contiene el estado al que se transiciona al leer el caracter correspondiente
-;;:El estado -1 corresponde al estado trampa
 
-;;; (define dfa- 
-;;; (list 
-;;; (list 
-;;; (list 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 0
-;;; (list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 1
-;;; (list 1, 1, 2, ..., n) ;Estado ...
-;;; )
-;;; (list 1) <- Aqui van los estados finales
-;;; )
-;;; )
 
-;;Tabla de estados - Multiplicación
 (define dfa-mult
 (list 
 (list 
-(list 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 0
-(list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 1
+(list 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1)
+(list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1)
 )
-(list 1) ;Estado final 1
+(list 1)
 )
 )
 
  
- ;;Tabla de estados - Identificador 
+ 
 (define dfa-iden
 (list 
 (list
-(list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 1 1 1 1 -1 -1) ;Estado 0
-(list -1 -1 -1 -1 -1 -1 -1 -1 1 1 -1 1 1 1 1 -1 -1) ;Estado 1
+(list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 1 1 1 1 -1 -1)
+(list -1 -1 -1 -1 -1 -1 -1 -1 -1  1  1 1 1 1 1 -1 -1)
 )
-(list 1) ;Estado final 1
+(list 1)
 )
 )
  
+
+
 ;;Tabla de estados - asignacion
 (define dfa-asig
 (list 
@@ -46,7 +32,7 @@
 (list -1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 0
 (list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 1
 )
-(list 1) ;Estado final 1
+(list 1)
 )
 )
 
@@ -57,7 +43,7 @@
 (list -1 -1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 0
 (list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 1
 )
-(list 1) ;Estado final 1
+(list 1)
 )
 )
 
@@ -68,7 +54,7 @@
 (list -1 -1 -1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 0
 (list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 1
 )
-(list 1) ;Estado final 1
+(list 1)
 )
 )
 
@@ -79,7 +65,7 @@
 (list -1 -1 -1 -1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 0
 (list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 1
 )
-(list 1) ;Estado final 1
+(list 1)
 )
 )
 
@@ -91,7 +77,7 @@
 (list -1 -1 -1 -1 -1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 0
 (list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 1
 )
-(list 1) ;Estado final 1
+(list 1)
 )
 )
 
@@ -102,7 +88,7 @@
 (list -1 -1 -1 -1 -1 -1 -1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 0
 (list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 1
 )
-(list 1) ;Estado final 1
+(list 1)
 )
 )
 
@@ -113,7 +99,7 @@
 (list -1 -1 -1 -1 -1 -1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 0
 (list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 1
 )
-(list 1) ;Estado final 1
+(list 1)
 )
 )
 
@@ -131,11 +117,12 @@
 (list -1 -1 -1 -1 -1 -1 -1 -1 7 -1 -1 -1 -1 -1 -1 -1 -1) ; Estado 6
 (list -1 -1 -1 -1 -1 -1 -1 -1 7 -1 -1 -1 -1 -1 -1 -1 -1) ; Estado 7
 )
-(list 4 7) ;Estados finales 4 y 7
+(list 1)
 )   
 )
 
-;;Tabla de estados - enteros
+
+
 (define dfa-ente
 (list 
 (list 
@@ -143,7 +130,7 @@
 (list -1 -1 -1 -1 -1 -1 -1 -1 2 -1 -1 -1 -1 -1 -1 -1 -1) ; Estado 1 (Estado de bucle) 
 (list -1 -1 -1 -1 -1 -1 -1 -1 2 -1 -1 -1 -1 -1 -1 -1 -1) 
 )
-(list 2) ;Estado final 2
+(list 1) ;Estados iniciales 1
 )
 )
 
@@ -151,11 +138,10 @@
 (define dfa-come
 (list 
 (list 
-(list -1 -1 -1 -1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1)
-(list -1 -1 -1 -1 2 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ; Estado 0: Inicio de comentario
-(list 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2) ; Estado 1: Contenido del comentario (cualquier carácter es válido)
+(list -1 -1 -1 -1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ; Estado 0: Inicio de comentario
+(list 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1) ; Estado 1: Contenido del comentario (cualquier carácter es válido)
 )
-(list 2) ;Estado final 2
+(list 1)
 )
 )
 
@@ -166,36 +152,55 @@
 (list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 1 -1 -1 -1 -1 -1 -1) ;Estado 0
 (list -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1) ;Estado 1
 )
-(list 1) ;Estado final 1
+(list 1)
 )
 )
 
-;;;Se determina el grupo al que pertenece el caracter, cada numero representa un indice, osea un caracter
+
+
+
+
+
+
 (define (determina-grupo linea)
 (cond
-  [(equal? (string-ref linea 0) #\*) 0] ;*
-  [(equal? (string-ref linea 0) #\=) 1] ;=
-  [(equal? (string-ref linea 0) #\+) 2] ;+
-  [(equal? (string-ref linea 0) #\-) 3] ;-
-  [(equal? (string-ref linea 0) #\/) 4] ;/
-  [(equal? (string-ref linea 0) #\^) 5] ;^
-  [(equal? (string-ref linea 0) #\() 6] ;(
-  [(equal? (string-ref linea 0) #\)) 7] ;)
-  [(and (>= (char->integer (string-ref linea 0)) 48) (<= (char->integer (string-ref linea 0)) 57)) 8] ;numeros 0-9
-  [(equal? (string-ref linea 0) #\_) 9] ;_
-  [(equal? (string-ref linea 0) #\ ) 10] ;espacio
-  [(and (>= (char->integer (string-ref linea 0)) 65) (<= (char->integer (string-ref linea 0)) 90)) 11] ;letras mayusculas
-  [(and (>= (char->integer (string-ref linea 0)) 97) (<= (char->integer (string-ref linea 0)) 100)) 12] ;letras minusculas a-d
-  [(equal? (string-ref linea 0) #\e) 13] ;e
-  [(and (>= (char->integer (string-ref linea 0)) 102) (<= (char->integer (string-ref linea 0)) 122)) 14] ;letras minusculas f-z
-  [(equal? (string-ref linea 0) #\.) 15] ;.
-  [else 16] ;Resto de los caracteres
+  [(equal? (string-ref linea 0) #\*) 0]
+  [(equal? (string-ref linea 0) #\=) 1]
+  [(equal? (string-ref linea 0) #\+) 2]
+  [(equal? (string-ref linea 0) #\-) 3]
+  [(equal? (string-ref linea 0) #\/) 4]
+  [(equal? (string-ref linea 0) #\^) 5]
+  [(equal? (string-ref linea 0) #\() 6]
+  [(equal? (string-ref linea 0) #\)) 7]
+  [(equal? (string-ref linea 0) #\ ) 8]
+  [(and (>= (char->integer (string-ref linea 0)) 48) (<= (char->integer (string-ref linea 0)) 57)) 9]
+  [(equal? (string-ref linea 0) #\_) 10]
+  [(and (>= (char->integer (string-ref linea 0)) 65) (<= (char->integer (string-ref linea 0)) 90)) 11]
+  [(and (>= (char->integer (string-ref linea 0)) 97) (<= (char->integer (string-ref linea 0)) 100)) 12]
+  [(equal? (string-ref linea 0) #\e) 13]
+  [(and (>= (char->integer (string-ref linea 0)) 102) (<= (char->integer (string-ref linea 0)) 122)) 14]
+  [(equal? (string-ref linea 0) #\.) 15]
+  [else 16]
   )
 )
 
 
 
-;;;Dividimos la entrada en tokens
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 (define (recorre-dfa dfa linea)
 
   (define (ciclo estado tabla finales linea-reducida linea-original estado-final-alcanzado pos-aceptada pos-actual)
@@ -217,94 +222,88 @@
 
 
 
-;;;Se le asigna un grupo a cada token
+
+
+
+
+
+
+
+
+
 (define (tokens-en-linea-v2 linea)
  (if (not (string=? linea "")) 
 (cond
   [(recorre-dfa dfa-iden linea) (begin
-                                    (display "Variable: ")
-                                    (displayln (recorre-dfa dfa-iden linea))
-                                    (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-iden linea))))
+                                                           (displayln (recorre-dfa dfa-iden linea))
+                                                           (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-iden linea))))
                                                            )
                                                            ]
   [(recorre-dfa dfa-real linea) (begin
-                                    (display "Real: ")
-                                    (displayln (recorre-dfa dfa-real linea))
-                                    (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-real linea))))
+                                                                    (displayln (recorre-dfa dfa-real linea))
+                                                                    (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-real linea))))
                                                                     )
                                                                     ]
   [(recorre-dfa dfa-ente linea) (begin
-                                   (display "Entero: ")
-                                   (displayln (recorre-dfa dfa-ente linea))
-                                   (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-ente linea))))
+                                          (displayln (recorre-dfa dfa-ente linea))
+                                          (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-ente linea))))
                                                                     )
                                           ]
   [(recorre-dfa dfa-come linea) (begin
-                                  (display "Comentario: ")
                                   (displayln (recorre-dfa dfa-come linea))
                                   (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-come linea))))
                                                                     )
                                   ]
   [(recorre-dfa dfa-mult linea) (begin
-                                 (display "Multiplicación: ")
                                  (displayln (recorre-dfa dfa-mult linea))
-                                 (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-mult linea))))
+                          (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-mult linea))))
                                                                     )
                                  ]
   [(recorre-dfa dfa-asig linea) (begin
-                                 (display "Asignación: ")
-                                 (displayln (recorre-dfa dfa-asig linea))
-                                 (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-asig linea))))
+                               (displayln (recorre-dfa dfa-asig linea))
+                                                                    (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-asig linea))))
                                                                     )
                                ]
   [(recorre-dfa dfa-suma linea) (begin
-                                 (display "Suma: ")
                                  (displayln (recorre-dfa dfa-suma linea))
-                                 (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-suma linea))))
+                                                                    (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-suma linea))))
                                                                     )
                                  ]
   [(recorre-dfa dfa-rest linea) (begin
-                                 (display "Resta: ")
-                                 (displayln (recorre-dfa dfa-rest linea))
-                                 (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-rest linea))))
+                               (displayln (recorre-dfa dfa-rest linea))
+                                                                    (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-rest linea))))
                                                                     )
-                                 ]
+                               ]
 
   [(recorre-dfa dfa-divi linea) (begin
-                                 (display "Division: ")
-                                 (displayln (recorre-dfa dfa-divi linea))
-                                 (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-divi linea))))
+                               (displayln (recorre-dfa dfa-divi linea))
+                                                                    (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-divi linea))))
                                                                     )
                                ]
   [(recorre-dfa dfa-paiz linea) (begin
-                                 (display "Parentesis izquierdo: ")
                                  (displayln (recorre-dfa dfa-paiz linea))
-                                 (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-paiz linea))))
+                                                                    (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-paiz linea))))
                                                                     )
                                  ]
   [(recorre-dfa dfa-pade linea) (begin
-                                 (display "Parentesis derecho: ")
                                  (displayln (recorre-dfa dfa-pade linea))
-                                 (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-pade linea))))
+                                                                    (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-pade linea))))
                                                                     )
                                  ]
   [(recorre-dfa dfa-pote linea) (begin
-                                 (display "Potencia: ")
                                  (displayln (recorre-dfa dfa-pote linea))
-                                 (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-pote linea))))
+                                                                    (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-pote linea))))
                                                                     )
                                  ]
   [(recorre-dfa dfa-sepa linea) (begin
-                                (display "Separación: ")
                                 (displayln (recorre-dfa dfa-sepa linea))
-                                (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-sepa linea))))
+                                                                    (tokens-en-linea-v2 (substring linea (string-length (recorre-dfa dfa-sepa linea))))
                                                                     )
                                 ]
   [else (displayln "Error, carácter no reconocido" )]
   )
 " "
 )
-)
+  )
 
-(displayln "Testing token recognition:")
-(tokens-en-linea-v2 "x = 42 / 3.14 //This is a comment")
+ (tokens-en-linea-v2 "x = 42 * 3.14 //This is a comment")
